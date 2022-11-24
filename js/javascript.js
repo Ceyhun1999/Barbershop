aee8d79e805e38ab3ad8ebc475b7e209a256d2a8
Ceyhun Rzayev <rzayev-ceyhun-99@mail.ru>
2022-11-24 17:51:51 +0400

    feat: add header elemets

--- js/javascript.js
+++ js/javascript.js
@@ -1,3 +1,12 @@
-const bar = document.querySelector('.bar');
-const barItems = document.querySelectorAll('.bar span');
-bar.addEventListener('click', () => bar.classList.toggle('bar-active'));
+const root = document.documentElement,
+      bar = document.querySelector('.bar'),
+      barItems = document.querySelectorAll('.bar span'),
+      menu = document.querySelector('.menu');
+bar.addEventListener('click', () => {
+    const nav = document.querySelector('nav');
+    bar.classList.toggle('bar-active');
+    menu.classList.toggle('menu-active');
+    root.style.setProperty('--topDinamic', nav.offsetHeight  + "px")
+    console.log(nav.offsetHeight);
+})
+

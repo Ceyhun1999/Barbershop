c37b56907a5edd6d56f04b3ec86184a5e7190cba
Ceyhun Rzayev <rzayev-ceyhun-99@mail.ru>
2022-12-01 17:53:11 +0400

    add main

--- js/javascript.js
+++ js/javascript.js
@@ -1,12 +1,15 @@
 const root = document.documentElement,
       bar = document.querySelector('.bar'),
       barItems = document.querySelectorAll('.bar span'),
-      menu = document.querySelector('.menu');
+      menu = document.querySelector('.menu'),
+      contacts = document.querySelector('.contacts');
 bar.addEventListener('click', () => {
     const nav = document.querySelector('nav');
     bar.classList.toggle('bar-active');
     menu.classList.toggle('menu-active');
-    root.style.setProperty('--topDinamic', nav.offsetHeight  + "px")
-    console.log(nav.offsetHeight);
+    contacts.classList.toggle('contacts-active');
+    document.body.classList.toggle('body-active');
+    root.style.setProperty('--topDinamic', nav.offsetHeight  + "px");
+    root.style.setProperty('--topDinamicTwo', nav.offsetHeight + menu.offsetHeight  + "px");
 })
 
